2d7bfb89360bca3e0bce685317fc9dbb107f4fd4
tienvt <tienvt@rikkeisoft.com>
2019-06-14 10:31:43 +0700

    v3

--- custom-sw.js
+++ custom-sw.js
@@ -1,67 +1,45 @@
-// var dataCacheName = 'my-app-data-v1';
-// self.addEventListener('fetch', function(e) {
-//   console.log('[ServiceWorker] Fetch', e.request.url);
-//   var dataUrl = 'execute-api.us-east-2.amazonaws.com';
-//   // trường hợp request lên địa chỉ api
-//   if (e.request.url.indexOf(dataUrl) > -1) {
-//     if(e.request.method === 'GET') {
-//         e.respondWith(
-//           // lấy dữ liệu mới nhất về, cache lại, và trả về cho người dùng
-//           caches.open(dataCacheName).then(function(cache) {
-//             return fetch(e.request).then(function(response) {
-//               cache.put('list-data', response.clone());
-//               return response;
-//             }).catch(err => {
-//               return cache.match('list-data').then(response => {
-//                 return response;
-//               })
-//             });
-//           })
-//       );
-//     } else if(e.request.method === 'POST') {
-//       return fetch(e.request).catch(err => {
-//         return fetch(e.request);
-//       });
-//     } else {
-//         return fetch(e.request);
-//     }
-//   }
-//   else {
-//     // ... còn không thì trả về cached asset files
-//     e.respondWith(
-//         caches.match(e.request).then(function(response) {
-//           return response || fetch(e.request);
-//         })
-//     );
-//   }
-// });
+var db;
+function createDB() {
+  var request = self.indexedDB.open("sync-data", 1);
+  request.onerror = function(event) {
+      console.log("error: " + event);
+  };
+  request.onsuccess = function(event) {
+      db = request.result;
+      console.log("success: "+ db);
+  };
+}
+function readDB() {
+  return new Promise((resolve, reject) => {
+      var request = db.transaction("data").objectStore("data").getAll();
+      request.onsuccess = function(event) {
+          resolve(event.target.result)
+      }
+      request.onerror = function(event) {
+          console.log("Unable to add data ");
+          reject(false)
+      }
+  });
+}
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    createDB()
+  );
+});
 self.addEventListener('sync', event => {
   if (event.tag == 'send-data') {
-    console.log('111111111111111111111')
-    // var db;
-    // var request = self.indexedDB.open("sync-data", 1);
-    // request.onsuccess = function(event) {
-    //   db = request.result;
-    //   console.log("success: "+ db);
-    // };
-    // var objectStore = db.transaction("data").objectStore("data");
-    // if(objectStore){
-    //   objectStore.openCursor().onsuccess = event => {
-    //     var cursor = event.target.result;
-    //     if (cursor) {
-    //       console.log(cursor);
-    //       cursor.continue();
-    //     }
-    //   }
-    // }
-    // event.waitUntil(
-    //   fetch('https://4ro9u5dkc7.execute-api.us-east-2.amazonaws.com/demo-pwa/sync', {
-    //       method: 'POST',
-    //       body: self.localStorage.getItem('offlineRequest'),
-    //       headers: { 'Content-Type': 'application/json' }
-    //   }).then(() => {
-    //     localStorage.removeItem('offlineRequest');
-    //   })
-    // );
+    event.waitUntil(
+      readDB().then(result => {
+        fetch('https://4ro9u5dkc7.execute-api.us-east-2.amazonaws.com/demo-pwa/sync', {
+          method: 'POST',
+          body: JSON.stringify(result),
+          headers: { 'Content-Type': 'application/json' }
+        }).then((event) => {
+          console.log(event)
+        }).catch(err => {
+          console.log(err)
+        })
+      })
+    )
   }
 });
