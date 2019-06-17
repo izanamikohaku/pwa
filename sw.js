b39e4635c9e37d26968128c1bd414427176844b0
tienvt <tienvt@rikkeisoft.com>
2019-06-17 17:52:48 +0700

    v11

--- sw.js
+++ sw.js
@@ -23,5 +23,6 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('https://fonts.gstatic.com/s/materialicons/.*'), new workbox.strategies.NetworkFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/pwa/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/pwa/'), new workbox.strategies.NetworkFirst ({}), 'GET')
